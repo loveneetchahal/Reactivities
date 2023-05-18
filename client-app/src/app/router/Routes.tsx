3cbbb7ea10d221eb264dc2e246907105a28838b2
Luka Djordjevic <luka.djordjevic_emplc>
2023-05-18 17:53:56 +0200

    buggy controller

--- client-app/src/app/router/Routes.tsx
+++ client-app/src/app/router/Routes.tsx
@@ -1,28 +1,27 @@
-import {createBrowserRouter, Navigate, RouteObject} from "react-router-dom";
+import { createBrowserRouter, Navigate, RouteObject } from "react-router-dom";
 import App from "../layout/App";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
 import NotFound from "../../features/errors/NotFound";
-import React from "react";
 import ServerError from "../../features/errors/ServerError";
 import LoginForm from "../../features/users/LoginForm";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
-        element: <App/>,
+        element: <App />,
         children: [
-            {path: 'activities', element: <ActivityDashboard/>},
-            {path: 'activities/:id', element: <ActivityDetails/>},
-            {path: 'createActivity', element: <ActivityForm key={'create'}/>},
-            {path: 'manage/:id', element: <ActivityForm key={'manage'}/>},
-            {path: 'errors', element: <TestErrors/>},
-            {path: 'not-found', element: <NotFound/>},
-            {path: 'login', element: <LoginForm />},
-            {path: 'server-error', element: <ServerError/>},
-            {path: '*', element: <Navigate replace to={'/not-found'}/>},
+            { path: 'activities', element: <ActivityDashboard /> },
+            { path: 'activities/:id', element: <ActivityDetails /> },
+            { path: 'createActivity', element: <ActivityForm key={'create'} /> },
+            { path: 'manage/:id', element: <ActivityForm key={'manage'} /> },
+            { path: 'errors', element: <TestErrors /> },
+            { path: 'not-found', element: <NotFound /> },
+            { path: 'login', element: <LoginForm /> },
+            { path: 'server-error', element: <ServerError /> },
+            { path: '*', element: <Navigate replace to={'/not-found'} /> },
         ]
     }
 ]
